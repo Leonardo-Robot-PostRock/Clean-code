57aa09b95c747a9d19cb3d495bf136bc5b6b7408
Leonardo <leonardo38361@gmail.com>
2023-11-29 20:51:14 -0300

    problemática de la herencia

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,13 +1,60 @@
 (() => {
-    //No aplicando el principio de responsabilidad única
+
+    // No aplicando el principio de responsabilidad única
 
     type Gender = 'M' | 'F';
 
     class Person {
-        constructor(name: string, gender: Gender, birthDate: Date) {}
+        constructor(
+            public name: string,
+            public gender: Gender,
+            public birthdate: Date
+        ) { }
     }
 
-    const newPerson = new Person('Leonardo', 'M', new Date('1994-06-22'))
-    console.log({ newPerson });
+    class User extends Person {
+        private lasteAccess: Date;
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ) {
+            super(name, gender, birthdate);
+            this.lasteAccess = new Date();
+        }
 
-})();
+        checkCredentials() {
+            return true
+        }
+    }
+
+    class UserSetting extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(email, role, name, gender, birthdate)
+        }
+    }
+
+    const userSetting = new UserSetting(
+        'usr/home',
+        '/home',
+        'leonardo@gmail',
+        'Admin',
+        'Leornardo',
+        'M',
+        new Date('1994-06-22')
+    )
+
+    console.log({ userSetting });
+
+
+})()
